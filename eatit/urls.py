19a527fe7953235b982d41f925f800f80c78dba0
JaySon <tshent@qq.com>
2014-10-21 19:16:18 +0800

    fix bug

--- eatit/urls.py
+++ eatit/urls.py
@@ -8,6 +8,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'eatit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
