b5db6a0a74b4722ff638155a25a63789f1c541a0
wayonglee <wayonglee@foxmail.com>
2014-11-10 15:29:53 +0800

    Revert "fix bug"
    This reverts commit 19a527fe7953235b982d41f925f800f80c78dba0.

--- eatit/urls.py
+++ eatit/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'eatit.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^/', include(admin.site.urls)),
     url(r'^admin/', include(admin.site.urls)),
 )
