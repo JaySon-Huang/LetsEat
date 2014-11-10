ff1d668efdc766b1bc8fc0a37bc54e6aefabcba6
wayonglee <wayonglee@foxmail.com>
2014-11-10 17:33:40 +0800

    定义shop字段

--- shop/models.py
+++ shop/models.py
@@ -3,12 +3,25 @@ from django.db import models
 
 class ShopKeeperModel(models.Model):
 	'''店长'''
-	pass
+	name = models.CharField(max_length=20)
+	phone = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+	account = models.CharField(max_length=15)
+	password = models.CharField(max_length=15)
 
 class ShopModel(models.Model):
 	'''店铺'''
-	pass
+	name = models.CharField(max_length=20)
+	address = models.CharField(max_length=50)
+	address = models.CharField(max_length=50)
+	shoptype = models.CharField(max_length=15)
+	miniprice = models.FloatField()
+	announcement = models.CharField(max_length=200)
+	status = models.CharField(max_length=5)
 
 class CuisineModel(models.Model):
 	'''菜式'''
-	pass
+	name = models.CharField(max_length=20)
+	price = models.FloatField()
+	grade = models.FloatField()
+	salesvolume = models.PositiveSmallIntegerField()
