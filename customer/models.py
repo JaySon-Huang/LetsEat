ac6b81ea41f18968724ba84072ce611f364ae264
JaySon <tshent@qq.com>
2014-11-13 21:49:44 +0800

    添加订单状态和评分的选项内容，限定字段内容

--- customer/models.py
+++ customer/models.py
@@ -14,8 +14,16 @@ class CustomerModel(models.Model):
 
 class OrderModel(models.Model):
 	'''订单'''
+
+	# 订单的不同状态
+	STATUS_OF_ORDER_CHOICES(
+		(1,'已下单'),
+		(2,'餐厅已确认'),
+		(3,'成功交易'),
+	)
+
 	time = models.DateTimeField()
-	status = models.CharField(max_length=5)
+	status = models.IntegerField(max_length=5)
 	shop = models.ForeignKey(ShopKeeperModel,related_name = "order_shop")
 	cusine = models.ManyToManyField(CuisineModel,related_name = "order_cuisine")
 	customer = models.ForeignKey(CustomerModel,related_name = "order_customer")
@@ -23,7 +31,18 @@ class OrderModel(models.Model):
 
 class CommentModel(models.Model):
 	'''评论'''
-	grade = models.FloatField()
+
+	# 评分的等级
+	GRADE_OF_CUISINE_CHOICES = (
+		(0.0,'0.0'),(0.5,'0.5'),
+		(1.0,'1.0'),(1.5,'1.5'),
+		(2.0,'2.0'),(2.5,'2.5'),
+		(3.0,'3.0'),(3.5,'3.5'),
+		(4.0,'4.0'),(4.5,'4.5'),
+		(5.0,'5.0'),
+	)
+
+	grade = models.FloatField(choices=GRADE_OF_CUISINE_CHOICES)
 	message = models.CharField(max_length=200)
 	cuisine = models.ForeignKey(CuisineModel,related_name = "comment_cuisine")
 	customer = models.OneToOneField(CustomerModel,related_name = "comment_customer")
