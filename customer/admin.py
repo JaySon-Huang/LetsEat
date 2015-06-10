43da372ba214e82b930894940c05f67dcae175b3
JaySon-Huang <jayson.hjs@gmail.com>
2015-06-10 16:01:21 +0800

    update admin

--- customer/admin.py
+++ customer/admin.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
-from .models import CustomerModel,OrderModel,CommentModel
+from .models import CustomerModel, OrderModel, CommentModel
 
+
+@admin.register(CustomerModel)
 class CustomerModelAdmin(admin.ModelAdmin):
-    list_display=('name','address','phone','alternatephone')
-    search_fields =('name','phone')
+    list_display = ('name', 'address', 'phone', 'alternatephone')
+    search_fields = ('name', 'phone')
 
-admin.site.register(CustomerModel,CustomerModelAdmin)
 
+@admin.register(OrderModel)
 class OrderModelAdmin(admin.ModelAdmin):
-    list_display=('id','time','status')
-    date_hierarchy ='time'
-    
-admin.site.register(OrderModel,OrderModelAdmin)
+    list_display = ('id', 'time', 'status')
+    date_hierarchy = 'time'
 
+
+@admin.register(CommentModel)
 class CommentModelAdmin(admin.ModelAdmin):
-    list_display=('cuisine','grade','message')
-    
-admin.site.register(CommentModel,CommentModelAdmin)
-
+    list_display = ('cuisine', 'grade', 'message')
--- shop/admin.py
+++ shop/admin.py
@@ -2,24 +2,33 @@ from django.contrib import admin
 from .models import ShopKeeperModel, ShopModel, CuisineModel
 
 
+class ShopKeeperInline(admin.StackedInline):
+    model = ShopKeeperModel
+    extra = 1
+
+
+class CuisineInline(admin.StackedInline):
+    model = CuisineModel
+    extra = 1
+
+
+@admin.register(ShopKeeperModel)
 class ShopKeeperModelAdmin(admin.ModelAdmin):
-    list_display = ('name','phone','shop')
-    search_fields = ('name','phone')
-
-admin.site.register(ShopKeeperModel,ShopKeeperModelAdmin)
+    list_display = ('name', 'phone', 'shop')
+    search_fields = ('name', 'phone')
 
 
+@admin.register(ShopModel)
 class ShopModelAdmin(admin.ModelAdmin):
-    list_display = ('name','shoptype','miniprice','status','announcement')
-    search_fields = ('name','shoptype','miniprice','status')
-    list_filter = ('address',)
+    list_display = ('name', 'shoptype', 'miniprice', 'status', 'announcement')
+    search_fields = ('name', 'shoptype', 'miniprice', 'status')
+    list_filter = ('address', )
 
-admin.site.register(ShopModel,ShopModelAdmin)
+    inlines = [ShopKeeperInline, CuisineInline]
 
 
+@admin.register(CuisineModel)
 class CuisineModelAdmin(admin.ModelAdmin):
-    list_display = ('name','price','grade','salesvolume','shop')
+    list_display = ('name', 'price', 'grade', 'salesvolume', 'shop')
     search_fields = ('name',)
     list_filter = ('shop',)
-
-admin.site.register(CuisineModel,CuisineModelAdmin)
